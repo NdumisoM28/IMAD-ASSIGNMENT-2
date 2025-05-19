4f77530260a2a0c615546c801d0bbaff9b5a5113
lab_services_student <ST10484494@vcconnect.edu.za>
2025-05-19 16:06:02 +0200

    back end coding

--- app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity.kt
@@ -1,12 +1,17 @@
 package vcmsa.ci.myimadassignment2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var startButton : Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,7 +20,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
+        }
+        startButton = findViewById(R.id.startButton)
+           startButton.setOnClickListener{
+                // Create an explicit intent to start SecondActivity
+                val intent = Intent(this , MainActivity2::class.java)
+                startActivity(intent) // Start the activity
+            }
         }
     }
-}
--- app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity2.kt
+++ app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity2.kt
@@ -1,12 +1,21 @@
 package vcmsa.ci.myimadassignment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+abstract class MainActivity2 : AppCompatActivity() , View.OnClickListener {
+    private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,34 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        questionTextView = findViewById(R.id.textView2)
+        trueButton = findViewById(R.id.truebutton2)
+        falseButton = findViewById(R.id.falsebutton3)
+        nextButton = findViewById(R.id.NQbutton10)
+
+        trueButton.setOnClickListener(this)
+        falseButton.setOnClickListener(this)
+        nextButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.truebutton2 -> {
+                // Nelson Mandela was NOT imprisoned in Alcatraz in 1956, so "True" is incorrect.
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.falsebutton3 -> {
+                // Nelson Mandela was NOT imprisoned in Alcatraz in 1956, so "False" is correct.
+                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.NQbutton10 -> {
+                    // Create an Intent to navigate to MainActivity3
+                    val intent = Intent(this, MainActivity3::class.java)
+            }
+        }
+
     }
 }
