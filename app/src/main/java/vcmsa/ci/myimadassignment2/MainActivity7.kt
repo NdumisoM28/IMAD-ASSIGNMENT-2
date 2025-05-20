48bdcb14983a3f1072c02b7a58a9875dfbb5806c
NdumisoM28 <ST10484494@vcconnect.edu.za>
2025-05-20 20:13:28 +0200

    more backend coding

--- app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity7.kt
+++ app/src/main/java/vcmsa/ci/myimadassignment2/MainActivity7.kt
@@ -1,12 +1,17 @@
 package vcmsa.ci.myimadassignment2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity7 : AppCompatActivity() {
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,12 @@ class MainActivity7 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val score = intent.getIntExtra("finalScore", 0)  // 0 is the default if no score was passed.  Use the key "finalScore"
+        val scoreText = findViewById<TextView>(R.id.scoreText)
+
+        scoreText.text = "Your Score: $score"  //  Use string interpolation for a clear display.
+
     }
 }
+
+
